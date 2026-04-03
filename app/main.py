efff0fa50b6a0bc8e34fbb8ca46bdcaf8b20448f
hll4 <nvinogradov721@gmail.com>
2026-04-04 01:45:50 +0300

    фикс https без бд

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from app.admin import setup_admin
+from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
 
 app = FastAPI(title="API")
+
+app.add_middleware(ProxyHeadersMiddleware, trusted_hosts="*")
 
 setup_admin(app)
 
